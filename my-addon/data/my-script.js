92a60f2fa66a7fa1bb28798e7bc717ea0b7e4f60
Quirio <Quirio123@gmail.com>
2014-12-26 19:46:05 +0100

    Empezado el parser

--- my-addon/data/my-script.js
+++ my-addon/data/my-script.js
@@ -3,14 +3,32 @@ var textarea = document.getElementsByTagName("textarea");
 
 for(var i=0; i<inputs.length; i++){
     if (inputs[i].type == "text")
-        inputs[i].onclick = Analizador;  
+        inputs[i].onkeyup = Analizador;  
 }
 
 for(var i=0; i<textarea.length; i++){
-   textarea[i].onclick = Analizador;  
+   textarea[i].onkeyup = Analizador;  
 }
 
  
 function Analizador(){
-    alert("Me has seleccionado");
+    var Texto = document.activeElement.value;
+    var Separado;
+    var PalabraAnterior;
+    var PalabraActual;
+    var PalabraSiguiente;
+  
+    
+    if(Texto.charAt(Texto.length-1) == " "){
+        Separado = Texto.split(" ");
+        if(Separado.length >= 2){    
+            PalabraActual = Separado[Separado.length-2];
+            PalabraAnterior = Separado[Separado.length-3];
+            console.log(PalabraActual + " s "  + PalabraAnterior + " s");
+        }
+                
+        else{
+                
+        }
+    }
 }
